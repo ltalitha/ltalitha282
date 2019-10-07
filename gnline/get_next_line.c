a41ecd513b50dad553c706da9573059869aee374
ltalitha <askoldodinson845@gmail.com>
2019-10-07 17:58:11 +0300

    rjgrigtr

--- gnline/get_next_line.c
+++ gnline/get_next_line.c
@@ -6,56 +6,57 @@
 /*   By: ltalitha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 19:57:48 by ltalitha          #+#    #+#             */
-/*   Updated: 2019/09/24 21:07:16 by ltalitha         ###   ########.fr       */
+/*   Updated: 2019/10/07 17:50:28 by ltalitha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*what_is_in_remain(char *remain, char **line)
+char				*what_is_in_remain(char *remain, char **line)
 {
-    char	*sigh_of_new_string;
+	char			*sigh_of_new_string;
 
-    sigh_of_new_string = NULL;
-    if (remain)
-        if ((sigh_of_new_string = ft_strchr(remain, '\n')))
-        {
-            *sigh_of_new_string = '\0';
-            *line = ft_strdup(remain);
-            ft_strcpy(remain, ++sigh_of_new_string);
-        }
-        else
-        {
-            *line = ft_strdup(remain);
-            ft_strclr(remain);
-        }
-    else
-        *line = ft_strnew(1);
-    return (sigh_of_new_string);
+	sigh_of_new_string = NULL;
+	if (remain)
+		if ((sigh_of_new_string = ft_strchr(remain, '\n')))
+		{
+			*sigh_of_new_string = '\0';
+			*line = ft_strdup(remain);
+			ft_strcpy(remain, ++sigh_of_new_string);
+		}
+		else
+		{
+			*line = ft_strdup(remain);
+			ft_strclr(remain);
+		}
+	else
+		*line = ft_strnew(0);
+	return (sigh_of_new_string);
 }
+
 int					get_next_line(const int fd, char **line)
 {
-    static char		*remain;
-    char			buffer[BUFF_SIZE + 1];
-    char			*sign_of_new_string;
-    char			*tmp;
-    int				what_read;
+	static char		*remain;
+	char			buffer[BUFF_SIZE + 1];
+	char			*sign_of_new_string;
+	char			*tmp;
+	int				what_read;
 
-    if (fd < 0 || !line || read(fd, buffer, 0) < 0)
-        return (-1);
-    sign_of_new_string = what_is_in_remain(remain, line);
-    while (!sign_of_new_string && (what_read = read(fd, buffer, BUFF_SIZE)))
-    {
-        buffer[what_read] = '\0';
-        if ((sign_of_new_string = ft_strchr(buffer, '\n')))
-        {
-            *sign_of_new_string = '\0';
-            sign_of_new_string++;
-            remain = ft_strdup(sign_of_new_string);
-        }
-        tmp = *line;
-        *line = ft_strjoin(*line, buffer);
-        free(tmp);
-    }
-    return ((what_read || ft_strlen(remain) || ft_strlen(*line)) ? 1 : 0);
+	if (fd < 0 || !line || read(fd, buffer, 0) < 0)
+		return (-1);
+	sign_of_new_string = what_is_in_remain(remain, line);
+	while (!sign_of_new_string && (what_read = read(fd, buffer, BUFF_SIZE)))
+	{
+		buffer[what_read] = '\0';
+		if ((sign_of_new_string = ft_strchr(buffer, '\n')))
+		{
+			*sign_of_new_string = '\0';
+			sign_of_new_string++;
+			remain = ft_strdup(sign_of_new_string);
+		}
+		tmp = *line;
+		*line = ft_strjoin(*line, buffer);
+		free(tmp);
+	}
+	return ((what_read || ft_strlen(*line)) ? 1 : 0);
 }
--- gnline/get_next_line.h
+++ gnline/get_next_line.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltalitha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 17:11:15 by ltalitha          #+#    #+#             */
+/*   Updated: 2019/10/07 17:41:30 by ltalitha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#include <unistd.h> // read
-#include <fcntl.h> // open
-#include <stdio.h> // printf
-#include <stdlib.h>
-#include "libft/libft.h"
-# define BUFFER_SIZE 50
 
-int get_next_line(const int fd, char **line);
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 1000
+
+int				get_next_line(const int fd, char **line);
 
 #endif
