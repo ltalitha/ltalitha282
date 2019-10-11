243abedda3feb687a07590ce510d14095e43b9f3
ltalitha <askoldodinson845@gmail.com>
2019-10-11 18:20:59 +0300

    hmmm

--- gnline/get_next_line.c
+++ gnline/get_next_line.c
@@ -6,57 +6,85 @@
 /*   By: ltalitha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 19:57:48 by ltalitha          #+#    #+#             */
-/*   Updated: 2019/10/07 17:50:28 by ltalitha         ###   ########.fr       */
+/*   Updated: 2019/10/11 18:15:46 by ltalitha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char				*what_is_in_remain(char *remain, char **line)
+t_arr		*newlist(const int fd)
 {
-	char			*sigh_of_new_string;
+	t_arr	*new;
 
-	sigh_of_new_string = NULL;
-	if (remain)
-		if ((sigh_of_new_string = ft_strchr(remain, '\n')))
-		{
-			*sigh_of_new_string = '\0';
-			*line = ft_strdup(remain);
-			ft_strcpy(remain, ++sigh_of_new_string);
-		}
-		else
-		{
-			*line = ft_strdup(remain);
-			ft_strclr(remain);
-		}
-	else
-		*line = ft_strnew(0);
-	return (sigh_of_new_string);
+	if (!(new = (t_arr *)malloc(sizeof(t_arr))))
+		return (NULL);
+	new->fd = fd;
+	new->rest = ft_strnew(BUFF_SIZE);
+	new->next = NULL;
+	return (new);
 }
 
-int					get_next_line(const int fd, char **line)
+char		*checkrest(char **p_n, char *rest)
 {
-	static char		*remain;
-	char			buffer[BUFF_SIZE + 1];
-	char			*sign_of_new_string;
-	char			*tmp;
-	int				what_read;
+	char *str;
 
-	if (fd < 0 || !line || read(fd, buffer, 0) < 0)
-		return (-1);
-	sign_of_new_string = what_is_in_remain(remain, line);
-	while (!sign_of_new_string && (what_read = read(fd, buffer, BUFF_SIZE)))
+	if ((*p_n = ft_strchr(rest, '\n')) != NULL)
 	{
-		buffer[what_read] = '\0';
-		if ((sign_of_new_string = ft_strchr(buffer, '\n')))
+		str = ft_strsub(rest, 0, *p_n - rest);
+		ft_strcpy(rest, ++(*p_n));
+	}
+	else
+	{
+		str = ft_strnew(ft_strlen(rest) + 1);
+		ft_strcat(str, rest);
+		ft_strclr(rest);
+	}
+	return (str);
+}
+
+int			get_line(const int fd, char **line, char *rest)
+{
+	char			buf[BUFF_SIZE + 1];
+	char			*p_n;
+	char			*tmp;
+	int				rd;
+
+	p_n = NULL;
+	rd = 1;
+	*line = checkrest(&p_n, rest);
+	while (p_n == 0 && ((rd = read(fd, buf, BUFF_SIZE)) != 0))
+	{
+		buf[rd] = '\0';
+		if ((p_n = ft_strchr(buf, '\n')) != NULL)
 		{
-			*sign_of_new_string = '\0';
-			sign_of_new_string++;
-			remain = ft_strdup(sign_of_new_string);
+			ft_strcpy(rest, ++p_n);
+			ft_strclr(--p_n);
 		}
 		tmp = *line;
-		*line = ft_strjoin(*line, buffer);
-		free(tmp);
+		if (!(*line = ft_strjoin(tmp, buf)) || rd < 0)
+			return (-1);
+		ft_strdel(&tmp);
 	}
-	return ((what_read || ft_strlen(*line)) ? 1 : 0);
+	return ((ft_strlen(*line) || ft_strlen(rest) || rd) ? 1 : 0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static t_arr	*list;
+	t_arr			*tmp;
+	int				ret;
+
+	if (fd < 0 || line == 0)
+		return (-1);
+	if (!list)
+		list = newlist(fd);
+	tmp = list;
+	while (tmp->fd != fd)
+	{
+		if (tmp->next == NULL)
+			tmp->next = newlist(fd);
+		tmp = tmp->next;
+	}
+	ret = get_line(fd, line, tmp->rest);
+	return (ret);
 }
